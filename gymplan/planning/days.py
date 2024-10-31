6512c9a2f904afd07733736477b681cfea70a1a3
hi <115565725+Mikeblackbelt@users.noreply.github.com>
2024-10-31 09:22:43 -0400

    Update days.py

--- gymplan/planning/days.py
+++ gymplan/planning/days.py
@@ -13,11 +13,29 @@ def getMGroupExercises(section:str,group:str,subgroup: str) -> dict:
       exercises = json.load(file)
    return exercises["Exercises"]
 
-class RoutineDay():
-   def __init__(self, muscles: list[list[str]], ciStructures: list[list[list[str],str,int]] = None): #me when nested lists
+defaultDirectVolumeRatios = {
+    'upperchest': 0.06,
+    'lowerchest': 0.06, 
+    'lats': 0.08, 
+    'midback': 0.06,
+    'frontdelts': 0.05, 
+    'sidedelts': 0.08, 
+    'reardelts': 0.04, 
+    'biceps': 0.09, 
+    'triceps': 0.08, 
+    'calves': 0.06, 
+    'quads': 0.12, 
+    'hamstrings': 0.08, 
+    'glutes': 0.06, 
+    'abs': 0.05,
+    'obliques': 0.03
+}
+
+class DayType():
+   def __init__(self, muscles: list[list[str,str,str]], ciStructures: list[list[list[str],str,int],str] = None): #me when nested lists
         """
         muscles: muscles trained in the day being constructed. the first item of each object should be the SECTION (i.e: upper body), the second should be GROUP (i,e: chest) and the third should be the subgrouP (i.e: upperchest)
-        ciStructures: list of the the distribution of compound/isolation exercises of each day. should be structured something like [[[[upperbody,chest,upperchest], c,3]. c represents compound, i represents isolation. strings. if None, one is automatically generated as per the logic in self.generate()
+        ciStructures: list of the the distribution of compound/isolation exercises of each day. should be structured something like [[[upperbody,chest,upperchest], c]]. c represents compound, i represents isolation. strings. if None, one is automatically generated as per the logic in self.generate()
         """
 
         self.muscles =  muscles 
@@ -30,11 +48,18 @@ class RoutineDay():
         """
         equipment = [x.lower() for x in equipment]
 
+        returnCompounds = []
+        returnIsos = []
+
         exercises = getMGroupExercises(muscle[0],muscle[1],muscle[2])
-        for item in exercises['Compound']: #the only thing i know for real
-            for equipmentItem in item['Equipment']: 
-                if equipmentItem.lower() not in equipment:
-                    pass #not finished
+        for item in exercises['Compound']:
+           if all(equipmentItem.lower() in equipment for equipmentItem in item['Equipment']):
+               returnCompounds.append(item)
+        
+        for item in exercises['Isolation']:
+            if all(equipmentItem.lower() in equipment for equipmentItem in item['Equipment']):\
+                returnIsos.append(item)
+        return [returnCompounds,returnIsos]
 
         #not finished
 
@@ -63,7 +88,28 @@ class RoutineDay():
             for muscle, mSets in zip(self.muscles,sets): #isolations
                 if mSets != 0:
                     ciStructure.append([muscle,'i',mSets])
-             
+    
+        exerciseList = []
+        else:
+            pass #finish this
+
+        
         for muscle in ciStructure:
-            ci = self.getCompIsol(muscle)
-            #not finished
+           ci = self.getCompIsol(muscle[0])
+           comp = ci[0]; iso = ci[1]
+           if muscle[1] = 'c':
+              exerciseList.append({random.choice(comp).keys()[0]:muscle[2]})
+           else:
+              exerciseList.append({random.choice(comp).keys()[0]:muscle[2]})
+
+        return exerciseList
+
+push = DayType(
+    [
+        ["upperbody", "chest", "upperchest"],
+        ["upperbody", "chest", "lowerchest"],
+        ["upperbody", "shoulders", "frontdelts"],
+        ["upperbody", "shoulders", "sidedelts"],
+        ["upperbody", "arms", "triceps"],
+    ] #add the other one later
+)
